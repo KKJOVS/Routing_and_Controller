9593d71e692a16b1aa94a4e6470896bccf17c4fa
KKJOVS <natrovez0914@gmail.com>
2024-07-04 10:09:10 +0800

    fixing my web.php

--- Routing_and_Controller/routes/web.php
+++ Routing_and_Controller/routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\AboutMeController;
 use App\Http\Controllers\SkillsController;
 use App\Http\Controllers\HobbiesController;
 
-Route::get('/about-me', [AboutMeController::class, 'show'])->name('about.me');
-Route::get('/skills', [SkillsController::class, 'show'])->name('skills');
-Route::get('/hobbies', [HobbiesController::class, 'show'])->name('hobbies');
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/about-me', [AboutMeController::class, 'aboutme']);
+Route::get('/skills', [SkillsController::class, 'skills']);
+Route::get('/hobbies', [HobbiesController::class, 'hobbies']);
